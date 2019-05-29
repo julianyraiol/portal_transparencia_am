cbc6ad789a51280ba3a9d2a1ff9e3bacaec1f223
julianyraiol <juliany@motorola.com>
2019-05-29 01:45:24 -0400

    feat: add methods that create folders to save files

--- portal_transparencia_am/portal_transparencia_am/pipelines.py
+++ portal_transparencia_am/portal_transparencia_am/pipelines.py
@@ -14,36 +14,55 @@ from scrapy.http import Request
 
 class FilePipeline(object):
 
+    def open_spider(self, spider):
+
+        self.current_path = Path('results')
+        if not self.current_path.exists():
+            self.current_path.mkdir()
+
     def process_item(self, item, spider):
 
+
         # Request pdf file
-        self.download_file(item, spider, type='pdf')
+        self.download_file(item, spider, type_request='pdf')
 
         # Request csv file
         if item.get('csv'):
-            self.download_file(item, spider, type='csv')
+            self.download_file(item, spider, type_request='csv')
 
         return item
 
-    def download_file(self, item, spider, type):
+    def download_file(self, item, spider, type_request):
 
         try:
-            request = Request(item[type]['url'])
+            request = Request(item[type_request]['url'])
             dfd = spider.crawler.engine.download(request, spider)
-            dfd.addBoth(self.save_file, item, type)
+            dfd.addBoth(self.save_file, item, type_request)
 
-            message = 'Saving {} file'.format(type)
+            message = 'Saving {} file'.format(type_request)
             print(message)
             return True
-        except e:
-            message = 'Failed to save {}'.format(type)
+        except:
+            message = 'Failed to save {}'.format(type_request)
             raise DropItem(message)
 
-    def save_file(self, response, item, type):
+    def save_file(self, response, item, type_request):
 
-        url = item[type]['url']
-        filename = url.split('/')[-1]
-        with open(filename, 'wb') as current_file:
+        name_folder = item.get('name')
+        path = self.current_path / name_folder
+
+        if not path.exists():
+            path.mkdir()
+
+        filepath = path / type_request
+        if not filepath.exists():
+            filepath.mkdir()
+
+        url = item[type_request]['url']
+        name = url.split('/')[-1]
+
+        filename = filepath / name
+        with filename.open('wb') as current_file:
             current_file.write(response.body)
 
         return filename
